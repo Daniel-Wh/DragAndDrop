ce1b942200c42edd9cd035afc9e3b210cad85ea9
Daniel Whitney <whitneyd@southwestern.edu>
2020-06-12 13:04:56 -0500

    added event listeners for drag/drop

--- script.js
+++ script.js
@@ -41,4 +41,48 @@ function createList() {
       listItems.push(listItem);
       draggable_list.appendChild(listItem);
     });
+  addEventListeners();
+}
+
+function dragStart() {
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+  console.log(dragStartIndex);
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove("over");
+}
+function dragEnter() {
+  this.classList.add("over");
+}
+function dragLeave() {
+  this.classList.remove("over");
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+
+  const itemtwo = listItems[toIndex].querySelector(".draggable");
+  listItems[fromIndex].appendChild(itemtwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
 }
